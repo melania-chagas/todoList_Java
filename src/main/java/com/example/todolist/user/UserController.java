c7c4de04899f3ab17c43927672bbda97b90d5511
Melania Chagas <melaniapchagas@gmail.com>
2023-10-18 10:42:25 -0300

    docs: no arquivo 'UserController.java'

--- src/main/java/com/example/todolist/user/UserController.java
+++ src/main/java/com/example/todolist/user/UserController.java
@@ -1,39 +1,39 @@
 package com.example.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
 
+/**
+ * UserController lida com as operações relacionadas a usuários(users).
+ */
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    // @Autowired: o objetivo principal é injetar dependências em um componente. As dependências podem ser outras classes, beans, serviços, repositórios, etc. Isso elimina a necessidade de criar manualmente instâncias das dependências e permite que o Spring cuide da gestão e criação dessas instâncias.
-
-    // @RequestBody: indica que um método de controlador deve receber os dados da solicitação HTTP no corpo (body) da solicitação. 
 
     @Autowired
     private IUserRepository userRepository;
 
+    /**
+     * Rota do tipo "POST" para criar um novo usuário.
+     * @param userModel Objeto do tipo 'UserModel' a ser criado como novo usuário;
+     * @return Um 'ResponseEntity' com a resposta da operação, que pode conter o usuário criado ou uma mensagem de erro.
+     */
     @PostMapping("/")
-    public ResponseEntity create(@RequestBody UserModel userModel) {
+    public ResponseEntity<Object> create(@RequestBody UserModel userModel) {
 
-        var passwordHashred = BCrypt.withDefaults()
+        var passwordHashed = BCrypt.withDefaults()
                 .hashToString(12, userModel.getPassword()
                 .toCharArray());
 
-        userModel.setPassword(passwordHashred);
+        userModel.setPassword(passwordHashed);
 
-        // Verifica se o 'username' já existe no banco de dados. Se não existir retorna 'null'
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if (user != null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username já existe");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username já cadastrado");
         }
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
