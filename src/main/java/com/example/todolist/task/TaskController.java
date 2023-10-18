7dfead624b671a817bcdbe6b3c83c2b39ba1105a
Melania Chagas <melaniapchagas@gmail.com>
2023-10-18 10:25:53 -0300

    docs: no arquivo 'TaskController.java' - otimizando importações + adicionando documentação no código

--- src/main/java/com/example/todolist/task/TaskController.java
+++ src/main/java/com/example/todolist/task/TaskController.java
@@ -4,58 +4,75 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.todolist.utils.Utils;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+// TaskController lida com as operações relacionadas às tarefas(tasks).
+
 @RestController
 @RequestMapping("/tasks")
+
 public class TaskController {
     @Autowired
     private ITaskRepository taskRepository;
 
+    /**
+     * Rota do tipo "POST" para cadastro de usuários.
+     * @param taskModel Objeto do tipo 'TaskModel' a ser criado;
+     * @param request Objeto do tipo 'HttpServletRequest' contendo informações da solicitação;
+     *  @return Um 'ResponseEntity' com a resposta da operação, que pode conter um objeto do tipo 'TaskModel' ou uma mensagem de erro.
+    */ 
     @PostMapping("/")
-    public ResponseEntity createNewTask(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity<Object> createNewTask(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID)idUser);
 
-        // Verifica se as datas das tasks fazem sentido.
         var currentDate = LocalDateTime.now();
         if(currentDate.isAfter(taskModel.getStartAt()) || 
             currentDate.isAfter(taskModel.getEndAt())){
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("A data de início/data de término - deve ser maior do que a data atual");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("A data de início/data de término - devem ser maior do que a data atual");
         }
 
         if(taskModel.getStartAt().isAfter(taskModel.getEndAt())){
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("A data de início deve ser menor do que a data de término");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("A data de início deve ser menor do que a data de término");
         }
 
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 
+
+    /**
+     * Rota do tipo "GET" para obter a lista de tarefas associadas a um usuário.
+     * @param request Objeto do tipo 'HttpServletRequest' contendo informações da solicitação;
+     * @return Uma lista de objetos do tipo 'TaskModel'.
+    */
     @GetMapping("/")
-    public List<TaskModel> getTaskByIdUser(HttpServletRequest request) {
+    public List<TaskModel> getTaskByUserId(HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
         var tasks = this.taskRepository.findTasksByIdUser((UUID)idUser);
         return tasks;
     }
 
+
+    /**
+     * Rota do tipo "PUT" para atualizar informações em uma tarefa existente com base em seu ID.
+     *
+     * @param taskModel Objeto do tipo 'TaskModel' com os dados atualizados;
+     * @param id        O ID da tarefa a ser atualizada;
+     * @param request   Objeto do tipo 'HttpServletRequest' contendo informações da solicitação;
+     * @return Um 'ResponseEntity' com a resposta da operação, que pode conter um objeto TaskModel atualizado ou uma mensagem de erro.
+    */
     @PutMapping("/{id}")
-    public ResponseEntity updateTaskById(
+    public ResponseEntity<Object> updateTaskById(
         @RequestBody TaskModel taskModel,
         @PathVariable UUID id,
         HttpServletRequest request) {
